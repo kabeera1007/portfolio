b6182643901cf3bcfe1749d1e2f6d69007c80e2d
kabeera1007 <way2mahadev@gmail.com>
2025-02-18 00:45:21 +0530

    added cards for ptojects

--- app.py
+++ app.py
@@ -3,20 +3,44 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/project1')
+def project1():
+    return render_template('project1.html')
+
+@app.route('/project2')
+def project2():
+    return render_template('project2.html')
+
+@app.route('/project3')
+def project3():
+    return render_template('project3.html')
+
+@app.route('/project4')
+def project4():
+    return render_template('project4.html')
+
+@app.route('/project5')
+def project5():
+    return render_template('project5.html')
+
+@app.route('/project6')
+def project6():
+    return render_template('project6.html')
+
+if __name__ == "__main__":
     app.run(debug=True)
