1c5b8a5833bc24838e9b104598b9a5e6844594cb
kabeera1007 <way2mahadev@gmail.com>
2025-02-19 01:59:06 +0530

    Third major commit

--- app.py
+++ app.py
@@ -18,29 +18,6 @@ def contact():
 def projects():
     return render_template('projects.html')
 
-@app.route('/project1')
-def project1():
-    return render_template('project1.html')
-
-@app.route('/project2')
-def project2():
-    return render_template('project2.html')
-
-@app.route('/project3')
-def project3():
-    return render_template('project3.html')
-
-@app.route('/project4')
-def project4():
-    return render_template('project4.html')
-
-@app.route('/project5')
-def project5():
-    return render_template('project5.html')
-
-@app.route('/project6')
-def project6():
-    return render_template('project6.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
